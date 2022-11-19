2d01e56d66099d9a37da698cc9ca6aedcd3c483d
Timothy Fang <56328137+Amighty-font@users.noreply.github.com>
2022-11-19 14:57:20 -0800

    added react-dom

--- sustainagoal/src/pages/Home.js
+++ sustainagoal/src/pages/Home.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React from "react";
+import "./Home.css";
+import { Container } from "react-bootstrap";
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <Container className="home">
+      <div className="py-3" />
+      <div className="row">
+        <div className="col">
+          <img className="rounded mx-auto d-block" src="https://brocku.ca/brock-news/wp-content/uploads/2022/09/GettyImages-1181366400-1600x1067.jpg?x70330" />
+        </div>
+        <div className="col align-self-center">
+          <p className="homeDesc">
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+      <div className="py-3" />
+      <div className="row partTwo">
+      <div className="col align-self-center">
+          <p className="homeDesc">
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div className="col">
+          <img className="rounded mx-auto d-block" src="https://brocku.ca/brock-news/wp-content/uploads/2022/09/GettyImages-1181366400-1600x1067.jpg?x70330" />
+        </div>
+      </div>
+    </Container>
   );
-}
+};
