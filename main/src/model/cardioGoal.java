f34f6267af535edc5d19d297ea1fea9c3f118f32
Yu Cheng Li <ychengli11065@gmail.com>
2022-11-19 12:23:27 -0800

    Changed Cardio goal name and added workout goals

--- main/src/model/cardioGoal.java
+++ main/src/model/cardioGoal.java
@@ -1,6 +1,6 @@
 package model;
 
-public class cardio_goal extends Goal{
+public class cardioGoal extends Goal{
     // Intensity 1 - 5
     // 1 being least intensive
     // 5 being most intensive
@@ -8,8 +8,23 @@ public class cardio_goal extends Goal{
     private String activity;
     private int calorie_count;
 
-    public cardio_goal() {
+    public cardioGoal() {
+        super();
+        intense = 0;
+        activity = "";
+        calorie_count = 0;
+    }
 
+    public int getIntense() {
+        return intense;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public int getCalorie_count() {
+        return calorie_count;
     }
 
     public void setActivity(String activity) {
