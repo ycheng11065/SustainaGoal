b2f80e5853f2fc4fc40c83bc2e70d67f361acbb7
Abdullah Eshaq <eshaq@student.ubc.ca>
2022-11-19 13:54:43 -0800

    Some ts apis

--- main/src_ts/model/Goal.ts
+++ main/src_ts/model/Goal.ts
@@ -3,11 +3,30 @@ export default class Goal {
     private _date: Date = new Date();
     private readonly _makeDate: Date;
     private readonly _deadline: Date;
+    private _done: number = 0; // 0 for not done, 1 for done
+    private _progress: number = 0;
 
     constructor() {
         this._makeDate = this._date;
         this._deadline = this._date;
         this._deadline.setMonth(this._deadline.getMonth() + 1);
+    }
+
+
+    get progress(): number {
+        return this._progress;
+    }
+
+    set progress(value: number) {
+        this._progress = value;
+    }
+
+    set done(value: number) {
+        this._done = value;
+    }
+
+    get done(): number {
+        return this._done;
     }
 
     get date(): Date {
