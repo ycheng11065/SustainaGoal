90505203cdc6eacf3fa5e36308008a5aa0a411a9
Abdullah Eshaq <eshaq@student.ubc.ca>
2022-11-19 11:29:08 -0800

    model

---
+++ main/src/model/Goal.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Goal {
+    private final Date makeDate;
+    private final Date Deadline;
+
+    public Goal() {
+        LocalDateTime date = LocalDateTime.now();
+
+    }
+}
