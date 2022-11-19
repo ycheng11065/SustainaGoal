096786c433d4aa2c01eaa7299b7b650a069af5d0
patrick <pgousse@student.ubc.ca>
2022-11-19 11:29:59 -0800

    init pages

---
+++ sustainagoal/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>Hello, I am your cat!</div>
+  );
+}
+
+export default Home;
---
+++ sustainagoal/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>Hello, I am your cat!</div>
+  );
+}
+
+export default Profile;
