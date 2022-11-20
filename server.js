1f67a2345bfee3df6be7eb232c93e663afb1e8b2
Yu Cheng Li <ychengli11065@gmail.com>
2022-11-19 19:40:45 -0800

    Deleted unecessary files, added node modules, env for strip api

---
+++ server.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").load()
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+console.log(stripeSecretKey)
+
+const express = require("express")
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.static("src/pages"))
+
+app.listen(3000)
