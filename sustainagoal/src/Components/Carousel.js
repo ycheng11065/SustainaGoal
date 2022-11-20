60d282d1ce815e6237903b16c3b0a7ee056e62f9
Timothy Fang <56328137+Amighty-font@users.noreply.github.com>
2022-11-20 02:01:53 -0800

    added stripecheckout

--- sustainagoal/src/Components/Carousel.js
+++ sustainagoal/src/Components/Carousel.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { useState } from "react";
-import {
-  CCarousel,
-  CCarouselItem,
-  CImage,
-} from "@coreui/react";
+import { useState, useRef } from "react";
+import { CCarousel, CCarouselItem, CImage } from "@coreui/react";
 import { Container } from "react-bootstrap";
 import "./Carousel.scss";
 import User from "../src_js/User";
+import Payment from '../Components/Payment'
 
 const Carousel = () => {
   const [clicked, setClicked] = useState(false);
-  function onClick() {
-    // console.log(User.waterGoal.goalAmount);
-    // User.addProgress("water", 2);
-    // console.log(User.waterGoal.goalProgress);
-    setClicked(!clicked);
-  }
+  const waterRef = useRef(null);
 
-  function addWaterGoal() {
-    User.addWaterGoal(5);
+  function setWaterGoal() {
+    User.addWaterGoal(waterRef.waterGoal);
+    console.log(waterRef.current.value);
   }
 
   function addProgress() {
@@ -29,26 +22,68 @@ const Carousel = () => {
 
   return (
     <Container>
-      <CCarousel controls indicators dark>
-      <CCarouselItem>
+      <Payment />
+      <CCarousel
+        transition="crossfade"
+        interval={false}
+        controls
+        indicators
+        dark
+      >
+        <CCarouselItem>
           <div class="py-3" />
-          <div className="row">
-            <div className="col">
-              <CImage
-                className="mx-auto d-block h-10"
-                src={"https://img.icons8.com/plasticine/512/plastic.png"}
-                alt="slide 1"
-              />
-            </div>
-            <div className="col">
-              <div className="descriptionContainer">
-                <h3 className="charityTitle">UNICEF USA</h3>
-                <p className="description">
-                Lorem ipsum dolor sit amet, cconsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+          {console.log(User.waterGoal)}
+          <Container>
+            <div className="row">
+              <div className="col">
+                <CImage
+                  className="mx-auto d-block h-10"
+                  src={"https://img.icons8.com/plasticine/512/plastic.png"}
+                  alt="slide 1"
+                />
+              </div>
+              <div className="col">
+                <div className="descriptionContainer">
+                  <h3 className="charityTitle">UNICEF USA</h3>
+                  <p className="description">
+                    Lorem ipsum dolor sit amet, cconsectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col" />
+                <div class="col">
+                  <form class="form-inline bottom">
+                    <div class="form-group form-inline mx-sm-3 mb-2">
+                      <input
+                        ref={waterRef}
+                        type="number"
+                        class="form-control inputBox"
+                        id="inputPassword2"
+                        placeholder="Daily Goal"
+                      />
+                      <button
+                        type="submit"
+                        onClick={setWaterGoal}
+                        class="btn submitBtn form-inline"
+                      >
+                        {" "}
+                        Set Goal{" "}
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="col" />
               </div>
             </div>
-          </div>
+          </Container>
         </CCarouselItem>
         <CCarouselItem>
           <div class="py-3" />
@@ -105,9 +140,6 @@ const Carousel = () => {
           </div>
         </CCarouselItem>
       </CCarousel>
-      {/* {clicked ? <a href="#" class="btn submitBtn active" onClick={onClick}role="button" aria-pressed="true">Add Goal</a> :       <a href="#" class="btn submitBtn disabled" onClick={onClick}role="button" aria-pressed="true">Goal Added  </a>} */}
-      <a href="#" class="btn submitBtn" onClick={addWaterGoal}role="button" aria-pressed="true">Add Goal</a>
-      <a href="#" class="btn submitBtn" onClick={addProgress}role="button" aria-pressed="true">Add Progress</a>
     </Container>
   );
 };
